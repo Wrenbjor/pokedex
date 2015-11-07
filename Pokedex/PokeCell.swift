bbaa60b9ee7b85506de118f5364b17bab9c65ac4
Wayne Renbjor <wayne_renbjor2@cable.comcast.com>
2015-11-07 14:42:51 -0500

    added sample pokes

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -15,6 +15,14 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 15.0
+        
+    }
+    
+    
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
         
